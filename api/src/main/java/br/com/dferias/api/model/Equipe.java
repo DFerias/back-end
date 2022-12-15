339996def0fa0c5fc8fe56f200ab4c34f27baff0
andre-voronhuk <andrevoronhuk@gmail.com>
2022-12-14 21:55:52 -0300

    FEAT: cadastro de equipe

--- api/src/main/java/br/com/dferias/api/model/Equipe.java
+++ api/src/main/java/br/com/dferias/api/model/Equipe.java
@@ -1,6 +1,8 @@
 package br.com.dferias.api.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.Setter;
 public class Equipe {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private Long id_lider;
