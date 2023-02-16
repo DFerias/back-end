11c582250f51d136efc8665b2ba8bad8495420be
andre-voronhuk <andrevoronhuk@gmail.com>
2023-02-15 22:03:13 -0300

    FEAT: Validacoes no cadastro de ferias
    - menos de 5 dias
    - incio em feriado

--- api/src/main/java/br/com/dferias/api/util/Validador.java
+++ api/src/main/java/br/com/dferias/api/util/Validador.java
@@ -2,17 +2,38 @@ package br.com.dferias.api.util;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.dferias.api.model.Funcionario;
+import br.com.dferias.api.service.FuncionarioService;
 
 @Service
 public class Validador {
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    public int getDiferencaEntreDatas(Date startDate, Date endDate) {
+        long ms = Math.abs(endDate.getTime() - startDate.getTime());
+        long dias = TimeUnit.DAYS.convert(ms, TimeUnit.MILLISECONDS);
+        return (int) dias;
+    }
+
+    public boolean isQuantidadeFeriasValido(Long id, Integer quantidade) {
+
+        Funcionario funcionario = funcionarioService.findById(id).get();
+        return funcionario.getSaldoFerias() >= quantidade;
+
+    }
 
     public static boolean isFeriadoNacional(Date data) {
         Calendar calendario = Calendar.getInstance();
         calendario.setTime(data);
         int mes = calendario.get(Calendar.MONTH);
-        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH) + 1;
+
         return (mes == Calendar.JANUARY && dia == 1) ||
                 (mes == Calendar.APRIL && dia == 21) ||
                 (mes == Calendar.MAY && dia == 1) ||
